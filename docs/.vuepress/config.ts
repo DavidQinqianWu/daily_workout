6f2e62fd12571b3a4c196fa6a1fd43307489223b
DavidQinqianWu <wqq1992324@gmail.com>
2022-12-08 00:17:21 +0800

    comment

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,42 @@
-import { defineUserConfig } from 'vuepress';
+import { defaultTheme, defineUserConfig } from 'vuepress';
+import { prismjsPlugin } from '@vuepress/plugin-prismjs';
 
 export default defineUserConfig({
     lang: 'zh-CN',
-    title: '学习笔记',
+    title: '武秦乾学习园地',
     description: '个人成长记录',
     base: '/daily_workout/',
+    plugins: [
+        prismjsPlugin({
+            // 配置项
+            preloadLanguages: ['markdown', 'typescript', 'javascript', 'jsdoc'],
+        }),
+    ],
+    theme: defaultTheme({
+        navbar: [
+            // NavbarItem
+            {
+                text: '算法',
+                link: '/algorithm/',
+            },
+            {
+                text: '精文',
+                link: '/text/',
+            },
+        ],
+        sidebar: {
+            '/algorithm/': [
+                {
+                    text: '算法',
+                    // children: ['/guide/README.md', '/guide/getting-started.md'],
+                },
+            ],
+            '/text/': [
+                {
+                    text: '精品文章',
+                    children: ['/reference/cli.md', '/reference/config.md'],
+                },
+            ],
+        },
+    }),
 });
---
+++ docs/index.md
@@ -0,0 +1,5 @@
+# 坚持学习的第 1 天
+
+### [算法篇](./algorithm/index.md)
+
+### [精品文章](./text/index.md)
