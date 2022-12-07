84b41c1fce93ccc4f34d30e757b19bdca8ee9113
武秦乾 <wuqinqian@xiaoyezi.com>
2022-12-07 20:04:22 +0800

    uppercaseKey.ts

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress';
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: '学习笔记',
+    description: '个人成长记录',
+    base: '/daily_workout/',
+});
